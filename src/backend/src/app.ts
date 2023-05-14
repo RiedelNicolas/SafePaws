94e64ee14cbd68b8090368c9fb5a3e663a3c6f26
Ivan Soriano <isoriano@fi.uba.ar>
2023-05-14 19:22:48 -0300

    Add mongo db

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import 'dotenv/config'
+const { MongoClient } = require("mongodb");
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri);
+
+app.get('/', async (req, res) => {
+  //Testing the atlas db connection using sample data
+  const database = client.db('sample_airbnb');
+  const houses = database.collection('listingsAndReviews');
+  const query = { name: 'Ribeira Charming Duplex' };
+  const house = await houses.findOne(query);
+
+  console.log(house);
+  res.send(house);
 });
 
 app.listen(port, () => {
