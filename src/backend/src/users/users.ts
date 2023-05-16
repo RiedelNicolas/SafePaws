985ee3be41f9632599bc6539fc0f5a31e38faf22
Ivan Soriano <isoriano@fi.uba.ar>
2023-05-16 00:08:50 -0300

    Implement basic user registration

--- src/backend/src/users/users.ts
+++ src/backend/src/users/users.ts
@@ -1,16 +1,36 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import { Request, Response } from 'express';
 import { client } from '../app';
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  //Testing the atlas db connection using sample data
-  const database = client.db('sample_airbnb');
-  const houses = database.collection('listingsAndReviews');
-  const query = { name: 'Ribeira Charming Duplex' };
-  const house = await houses.findOne(query);
 
-  res.send(house.name);
-//   res.send('User not found')
-})
+//Creates a new user if it does not already exist
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body; //TODO: Add schema validation
+
+    // Check if the user already exists
+    const users = client.db('SafePaws').collection('users'); //TODO: Remove this logic from each endpoint
+    const existingUser = await users.findOne({ username: username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = {
+      username,
+      password: hashedPassword,
+    };
+
+    await users.insertOne(newUser);
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+   console.error(error);
+   res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 module.exports = router
