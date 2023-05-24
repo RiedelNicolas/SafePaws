9b6ccdee79033a35bd166e220ca1fd4eba47f999
Ezequiel Zbogar <ezezbogar@gmail.com>
2023-05-23 21:55:29 -0300

    update firebase config

--- src/frontend/src/firebase/config.ts
+++ src/frontend/src/firebase/config.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +21,4 @@ const firebaseConfig = {
 export const FirebaseApp = initializeApp( firebaseConfig );
 export const FirebaseAuth = getAuth( FirebaseApp );
 export const FirebaseDB = getFirestore( FirebaseApp );
+export const FirebaseStorage = getStorage( FirebaseApp );
